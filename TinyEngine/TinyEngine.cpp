bf700e900485109bade936ddcdeca7a12800e782
tuchang <2248574642@qq.com>
2022-09-01 16:53:05 +0800

    新增平台无关性

--- TinyEngine/TinyEngine.cpp
+++ TinyEngine/TinyEngine.cpp
@@ -4,6 +4,24 @@
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
 	PSTR cmdLine, int showCmd)
 {
+//执行某函数获取平台
+//UINT GetPlatform()
+	UINT platform /*= GetPlatform()*/;
+
+#if Windows == platform
+	//执行初始化Windows的逻辑
+	//下述同理
+#endif
+
+#if Mac == platform
+#endif
+
+#if Android == platform
+#endif
+
+#if Ios == platform
+#endif 
+
 	DxApp instance(hInstance);
 	instance.Init();
 
